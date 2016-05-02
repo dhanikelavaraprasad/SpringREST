2cf4ac240bd6b79562da3f744a6cabceb4d8ed1c
Varaprasad <dhanikelavaraprasad@gmail.com>
2016-05-02 23:10:13 +0530

    Added CORSFilter and URIConstants

---
+++ src/main/java/com/ranga/util/URIConstants.java
@@ -0,0 +1,12 @@
+package com.ranga.util;
+
+public class URIConstants {
+	
+	public static final String DUMMY_EMP = "/emp/dummy";
+	public static final String GET_EMP = "/emp/{id}";
+	public static final String GET_ALL_EMP = "/emp/list";
+	public static final String CREATE_EMP = "/emp/create";
+	public static final String DELETE_EMP = "/emp/delete/{id}";
+	public static final String EDIT_EMP = "/emp/edit/{id}";
+
+}
